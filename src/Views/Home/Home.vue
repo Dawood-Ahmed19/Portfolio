a697988a03fc2c83436b79fd1682d3650ad5fc2b
Dawood Ahmed <gfxwork66@gmail.com>
2025-04-14 00:57:58 +0500

    fix: components route

--- src/Views/Home/Home.vue
+++ src/Views/Home/Home.vue
@@ -1,26 +1,9 @@
 <script setup>
 import Button from "../../components/Button/Button.vue";
-import MainImg from "../../assets/mainimg.png";
+import Hero from "../../Sections/Hero/Hero.vue";
 </script>
 
 <template>
   <!-- Hero Section -->
-  <section class="container w-[calc(100% - 2rem)] m-auto">
-    <div class="flex flex-row items-center justify-center xl:justify-between">
-      <span class="flex flex-col gap-4">
-        <h1 class="text-5xl font-semibold text-white">
-          Dawood is A <span class="text-mainPink">Full Stack Developer</span>
-        </h1>
-        <p class="text-gray-400 text-lg tracking-widest">
-          He engineers sleek, responsive websites with a creative touch
-        </p>
-        <Button>Contact me</Button>
-      </span>
-      <span
-        class="relative xl:inline-block hidden before:content-[url('./assets/Dots.png')] before:absolute before:top-[7rem] before:left-[18rem] before:rotate-12"
-      >
-        <img class="max-w-2xl w-full h-auto" :src="MainImg" alt="body img" />
-      </span>
-    </div>
-  </section>
+  <Hero />
 </template>
