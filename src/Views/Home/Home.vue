0a857173544551334b1db8603260a7cc07205517
Dawood Ahmed <gfxwork66@gmail.com>
2025-04-14 01:09:44 +0500

    fix: Hero section

--- src/Views/Home/Home.vue
+++ src/Views/Home/Home.vue
@@ -1,9 +1,13 @@
 <script setup>
 import Button from "../../components/Button/Button.vue";
 import Hero from "../../Sections/Hero/Hero.vue";
+import Work from "../../Sections/Work/Work.vue";
 </script>
 
 <template>
   <!-- Hero Section -->
   <Hero />
+
+  <!-- Work Section -->
+  <Work />
 </template>
