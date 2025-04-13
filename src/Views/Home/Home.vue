4df4d903f911292e78aaa7540bb57b0b81705c4f
Dawood Ahmed <gfxwork66@gmail.com>
2025-04-13 19:58:25 +0500

    feat: add social icons

--- src/Views/Home/Home.vue
+++ src/Views/Home/Home.vue
@@ -1,3 +1,25 @@
+<script setup>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+
+const icons = [
+  {
+    icon: faLinkedin,
+    path: "https://www.linkedin.com/in/dawood-ahmed-911742275/",
+  },
+  {
+    icon: faGithub,
+    path: "https://github.com/Dawood-Ahmed19",
+  },
+];
+</script>
+
 <template>
-  <h1 class="text-white">This is home page</h1>
+  <span class="absolute">
+    <li v-for="icon in icons" :key="icon.icon">
+      <a target="_blank" :href="icon.path">
+        <FontAwesomeIcon :icon="icon.icon" />
+      </a>
+    </li>
+  </span>
 </template>
